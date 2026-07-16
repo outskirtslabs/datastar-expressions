431b9a007c07346792ff30d806845610b799e279
Casey Link <unnamedrambler@gmail.com>
2026-07-16 09:43:45 +0200

    nix: add standard Clojure development flake

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "dev env";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    clj-helpers.url = "github:outskirtslabs/clojure-nix-locker-helpers";
+    clj-helpers.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    inputs@{
+      self,
+      devenv,
+      devshell,
+      clj-helpers,
+      ...
+    }:
+    let
+      package =
+        pkgs:
+        clj-helpers.lib.mkCljLib {
+          inherit pkgs;
+          name = "datastar-expressions";
+          version = "0.0.1";
+          src = ./.;
+          prepAliases = [
+            "dev"
+            "kaocha"
+            "build"
+          ];
+          prefetchAliases = [
+            "kaocha"
+            "dev:kaocha"
+          ];
+          checkCommand = "clojure -Srepro -M:kaocha";
+          gitRev = clj-helpers.lib.gitRev self;
+        };
+    in
+    devenv.lib.mkFlake ./. {
+      inherit inputs;
+      withOverlays = [
+        devshell.overlays.default
+        devenv.overlays.default
+      ];
+      packages = {
+        default = package;
+        # regenerates ./deps-lock.json: `nix run .#locker`
+        locker = pkgs: (package pkgs).locker;
+      };
+      devShell =
+        pkgs:
+        pkgs.devshell.mkShell {
+          imports = [
+            devenv.capsules.base
+            devenv.capsules.clojure
+          ];
+          # https://numtide.github.io/devshell
+          commands = [
+            # { package = pkgs.bazqux; }
+          ];
+          packages = [
+            (
+              if self ? packages then
+                self.packages.${pkgs.system}.locker
+              else
+                clj-helpers.packages.${pkgs.system}.deps-lock
+            )
+            # pkgs.foobar
+          ];
+        };
+    };
+}
